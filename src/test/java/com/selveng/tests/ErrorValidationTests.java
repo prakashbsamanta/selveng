19ad9eb3458f7fd9439f531a0505a42fc3d976ce
prakash.s <prakashbsamanta@gmail.com>
2024-02-01 14:06:47 +0530

    feat: add screenshot function

--- src/test/java/com/selveng/tests/ErrorValidationTests.java
+++ src/test/java/com/selveng/tests/ErrorValidationTests.java
@@ -2,10 +2,16 @@ package com.selveng.tests;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.selveng.testComponents.BaseTest;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 
@@ -19,9 +25,10 @@ public class ErrorValidationTests extends BaseTest {
     }
 
     @Test(dataProvider = "validatingInputFieldErrorsData")
-    public void validatingInputFieldErrors(HashMap<String, String> dataMap) {
+    public void validatingInputFieldErrors(HashMap<String, String> dataMap) throws IOException {
         System.out.println("Test Desc: " + dataMap.get("testDesc"));
         loginPage.loginApplication(dataMap.get("username"), dataMap.get("userPassword"));
+        getScreenShot(dataMap.get("testDesc"));
         String errorText = loginPage.getToastContainerText(loginPage.emailRequiredError);
         Assert.assertEquals(errorText, dataMap.get("errorText"));
     }
@@ -34,5 +41,15 @@ public class ErrorValidationTests extends BaseTest {
                 {dataObj.get(1)},
                 {dataObj.get(2)},
         };
+    }
+
+    public void getScreenShot(String testCaseName) throws IOException {
+        Instant instant = Instant.now();
+        long timeStampInMilli = instant.toEpochMilli();
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File ss = screenshot.getScreenshotAs(OutputType.FILE);
+        File file = new File("src/resources/screenshots/" + testCaseName + "_" + timeStampInMilli + ".png");
+        FileUtils.copyFile(ss, file);
+        System.out.println("Screenshot saved at: " + file);
     }
 }
